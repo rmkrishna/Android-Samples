80623178e287c2aab9e9e7d1d61aa17242e1232d
rmkrishna <rmkrishna.sacoe@gmail.com>
2019-05-25 15:39:39 +0530

    Added separate module for Android Q

--- Magnifier/app-q/src/main/java/com/rmkrishna/demo/magnifier/MainActivity.kt
+++ Magnifier/app-q/src/main/java/com/rmkrishna/demo/magnifier/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Rect
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.widget.Magnifier
 import androidx.annotation.RequiresApi
@@ -16,11 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        val magnifier = Magnifier(sampleText)
+        val magnifier = Magnifier.Builder(sampleText)
+            .setInitialZoom(2.0f)
+            .setElevation(20.0f)
+            .setClippingEnabled(true)
+            .setCornerRadius(10.0f * 3).build()
 
         var outRect = Rect()
-
         sampleText.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
@@ -30,8 +33,21 @@ class MainActivity : AppCompatActivity() {
                     sampleText.getLocationOnScreen(viewPosition)
                     outRect.offset(viewPosition[0], viewPosition[1])
 
+
                     if (outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                        magnifier.show(event.rawX - viewPosition[0], event.rawY - viewPosition[1])
+                        var currentXPosition = event.rawX - viewPosition[0]
+                        var currentYPosition = event.rawY - viewPosition[1]
+                        magnifier.show(
+                            currentXPosition,
+                            currentYPosition,
+                            currentXPosition,
+                            currentYPosition - (viewPosition[1].toFloat()/ 2)
+                        )
+
+//                        magnifier.show(
+//                            currentXPosition,
+//                            currentYPosition
+//                        )
                     } else {
                         magnifier.dismiss()
                     }
